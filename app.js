811b202b9ccfeca27bc11d1adfc6182a1701b7e3
sabrow <sabrow@hotmail.fr>
2022-05-07 10:51:20 +0200

    fenetre modale qui ne fonctionne pas

---
+++ app.js
@@ -0,0 +1,8 @@
+
+var myModal = document.getElementById('exampleModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
